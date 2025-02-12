a0caacbe9788aebea9530139446eaf1151835280
ukit-tokyo <t.yuki@ukit.tokyo>
2025-02-12 22:03:52 +0900

    add consumer

--- riverpod_tutorial/lib/main.dart
+++ riverpod_tutorial/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'activity.dart';
+import 'provider.dart';
+
 part 'main.g.dart';
 
 // We create a "provider", which will store a value (here "Hello world").
@@ -22,19 +25,42 @@ void main() {
   );
 }
 
-// Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
-class MyApp extends ConsumerWidget {
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Example')),
-        body: Center(
-          child: Text(value),
-        ),
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, child) {
+        final AsyncValue<Activity> activity = ref.watch(activityProvider);
+
+        print(activity);
+
+        return Center(
+          child: switch (activity) {
+            AsyncData(:final value) => Text('Activity: ${value.activity}'),
+            AsyncError() => const Text('Oops, something unexpected happened'),
+            _ => const CircularProgressIndicator(),
+          },
+        );
+      },
     );
   }
 }
+
+// Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
+// class MyApp extends ConsumerWidget {
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final String value = ref.watch(helloWorldProvider);
+
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('Example')),
+//         body: Center(
+//           child: Text(value),
+//         ),
+//       ),
+//     );
+//   }
+// }
