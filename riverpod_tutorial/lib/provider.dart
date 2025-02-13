8057b966cbbcdf48cf0dc77a7618a8d026b3a63a
ukit-tokyo <t.yuki@ukit.tokyo>
2025-02-13 23:53:10 +0900

    add todo

--- riverpod_tutorial/lib/provider.dart
+++ riverpod_tutorial/lib/provider.dart
@@ -7,12 +7,10 @@ part 'provider.g.dart';
 
 @riverpod
 Future<bool> result(Ref ref) async {
-  print("⭐️");
   final url = Uri.parse(
       'https://umayadia-apisample.azurewebsites.net/api/persons/Shakespeare');
   final response = await http.get(url);
 
-  print("⭐️⭐️");
   final json = jsonDecode(response.body);
   final success = json['success'] as bool;
   // 最終的にMapをActivityインスタンスに変換します。
--- riverpod_tutorial/lib/todo/provider.g.dart
+++ riverpod_tutorial/lib/todo/provider.g.dart
@@ -6,21 +6,20 @@ part of 'provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$resultHash() => r'6141ab531baadff791d16baf90ad8b8eec7b5bc4';
+String _$todoListHash() => r'4c1647aee72f472a17c4af603f012653abc74933';
 
-/// See also [result].
-@ProviderFor(result)
-final resultProvider = AutoDisposeFutureProvider<bool>.internal(
-  result,
-  name: r'resultProvider',
+/// See also [TodoList].
+@ProviderFor(TodoList)
+final todoListProvider =
+    AutoDisposeAsyncNotifierProvider<TodoList, List<Todo>>.internal(
+  TodoList.new,
+  name: r'todoListProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$resultHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$todoListHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef ResultRef = AutoDisposeFutureProviderRef<bool>;
+typedef _$TodoList = AutoDisposeAsyncNotifier<List<Todo>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
